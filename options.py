2e8d2c92f000b810f30b4cc443c3e1a4c0b2d25e
Gabriel Alencar <106896178+Coluno@users.noreply.github.com>
2024-12-10 16:43:55 -0300

    Update options.py

--- options.py
+++ options.py
@@ -269,9 +269,6 @@ def VaR():
 
     data = yf.download(escolha, start='2013-01-01', end='2025-01-01')
     
-    data.reset_index(inplace=True)
-    data.columns = data.columns.droplevel(1)
-    
     current_price = data['Adj Close'].iloc[-1]
 
     data_fim = st.date_input('Selecione a data final:', datetime.now())
