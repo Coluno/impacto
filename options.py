92d326e6985c232a4b376c457b459c48d0797e6e
Gabriel Alencar <106896178+Coluno@users.noreply.github.com>
2024-12-23 10:09:38 -0300

    Update options.py
       if "valor_simulado" not in st.session_state:
        st.session_state["valor_simulado"] = float(data['Close'].iloc[-1])
        valor_simulado = st.number_input("Qual valor deseja simular?",value=st.session_state["valor_simulado"],step=0.01)

--- options.py
+++ options.py
@@ -1239,9 +1239,9 @@ def monte_carlo():
     dias_simulados = calcular_dias_uteis(hoje, data_simulacao)
 
     # Input para o valor desejado para a simulação
-    #valor_simulado = st.number_input("Qual valor deseja simular?", min_value=float(data['Close'].min()), max_value=float(data['Close'].max()), step=0.01)
-    valor_simulado = st.number_input("Qual valor deseja simular?", value=float(data['Close'].iloc[-1]),step=0.01)
-    # Definir limites inferior e superior
+    if "valor_simulado" not in st.session_state:
+    st.session_state["valor_simulado"] = float(data['Close'].iloc[-1])
+    valor_simulado = st.number_input("Qual valor deseja simular?",value=st.session_state["valor_simulado"],step=0.01)
     limite_inferior = data['Close'].iloc[-1] - 10
     limite_superior = data['Close'].iloc[-1] + 10
 
