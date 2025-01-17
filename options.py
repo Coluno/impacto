ef747cbac45d1d674b45abf86831bb65aaf56a45
Gabriel Alencar <106896178+Coluno@users.noreply.github.com>
2025-01-17 11:58:11 -0300

    Update options.py

--- options.py
+++ options.py
@@ -1267,7 +1267,7 @@ def monte_carlo():
     limite_superior = data['Close'].iloc[-1] + 10
 
     # Simulação Monte Carlo
-    num_simulacoes = 100000
+    num_simulacoes = 999999
     simulacoes = simulacao_monte_carlo(data, media_retornos_diarios, desvio_padrao_retornos_diarios, dias_simulados, num_simulacoes, limite_inferior, limite_superior)
 
     if st.button("Simular"):
