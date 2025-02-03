9e98bd8fa0314a708e7d3a3c983b13fe26ffef7b
Gabriel Alencar <106896178+Coluno@users.noreply.github.com>
2025-02-03 13:47:27 -0300

    Update options.py

--- options.py
+++ options.py
@@ -804,14 +804,14 @@ def regressao_sugar():
         X_novo = pd.DataFrame([[log_dif_estoque, log_dif_oferta_demanda, log_estoque_uso, dif_log_usd_brl, dif_log_cl_f]],
                               columns=['Log_Diferencial_Estoque', 'Log_Diferencial_Oferta_Demanda', 'Log_Estoque_Uso', 'Dif_Log_USDBRL', 'Dif_Log_CL_F'])
 
-        previsoes = np.array([tree.predict(X_novo)[0] for tree in model.estimators_])
-        dif_log_sb_f_previsto = previsoes.mean()
-        dif_log_sb_f_min = np.percentile(previsoes, 25)
-        dif_log_sb_f_max = np.percentile(previsoes, 70)
+        #previsoes = np.array([tree.predict(X_novo)[0] for tree in model.estimators_])
+        #dif_log_sb_f_previsto = previsoes.mean()
+        #dif_log_sb_f_min = np.percentile(previsoes, 25)
+        #dif_log_sb_f_max = np.percentile(previsoes, 70)
         # Reverter log para previsão final
         sb_f_previsto = revert_log_diff(df['SB=F'].iloc[-1], dif_log_sb_f_previsto)
-        sb_f_min = revert_log_diff(df['SB=F'].iloc[-1], dif_log_sb_f_min)
-        sb_f_max = revert_log_diff(df['SB=F'].iloc[-1], dif_log_sb_f_max)
+        sb_f_min = revert_log_diff(df['SB=F'].iloc[-1], dif_log_sb_f_previsto - rmse)
+        sb_f_max = revert_log_diff(df['SB=F'].iloc[-1], dif_log_sb_f_previsto + rmse)
         
         st.write(f"### Preço previsto de SB=F: {sb_f_previsto:.2f} (Min: {sb_f_min:.2f}, Max: {sb_f_max:.2f})")
 ## parei aq
